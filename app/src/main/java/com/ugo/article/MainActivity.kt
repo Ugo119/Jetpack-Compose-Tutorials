b644dcf0e836b5a4377d1be39aa1e9e2e44225a0
ugoedukoya <ugochukwu@edukoya.com>
2022-09-13 10:48:25 +0100

    Completed Interacting With UI State Path - Tip Calculator

--- app/src/main/java/com/ugo/article/MainActivity.kt
+++ app/src/main/java/com/ugo/article/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.ugo.article
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.style.ClickableSpan
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -11,9 +13,11 @@ import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,11 +25,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ugo.article.ui.theme.ArticleTheme
+import java.text.NumberFormat
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,120 +41,76 @@ class MainActivity : ComponentActivity() {
         setContent {
             ArticleTheme {
                 // A surface container using the 'background' color from the theme
-                LemonadeApp()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TipTimeScreen()
+                }
             }
         }
     }
 }
 
 @Composable
-fun LemonadeApp() {
-    LemonadeScreens(
-        modifier = Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center)
+fun TipTimeScreen() {
+    var amountInput by remember { mutableStateOf("0") }
+    val amount = amountInput.toDoubleOrNull()?: 0.0
+    val tip = calculateTip(amount)
+    Column(
+        modifier = Modifier.padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.calculate_tip),
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        EditNumberField(
+            value = amountInput,
+            onValueChange = { amountInput = it }
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = stringResource(id = R.string.tip_amount, tip),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+    }
+}
+
+@Composable
+fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(text = stringResource(id = R.string.cost_of_service)) },
+        modifier = Modifier.fillMaxWidth(),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
     )
 }
 
-@Composable
-fun LemonadeScreens(modifier: Modifier = Modifier) {
-    var counter by remember { mutableStateOf(1) }
-    var squeezeCount by remember { mutableStateOf(0) }
-//    val imageResource = when(counter) {
-//        1 -> R.drawable.lemon_tree
-//        2 -> R.drawable.lemon_squeeze
-//        3 -> R.drawable.lemon_drink
-//        4 -> R.drawable.lemon_restart
-//        else -> R.drawable.lemon_tree
-//    }
-//    val stringText = when(counter) {
-//        1 -> R.string.first
-//        2 -> R.string.second
-//        3 -> R.string.third
-//        4 -> R.string.fourth
-//        else -> R.string.first
-//    }
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
-    ) {
-        when(counter) {
-            1 -> {
-                LemonTextWithImage(
-                    label = stringResource(id = R.string.first), 
-                    image = painterResource(id = R.drawable.lemon_tree)
-                ) {
-                    counter = 2
-                    squeezeCount = (2..4).random()
-                } 
-            }
-
-            2 -> {
-                LemonTextWithImage(
-                    label = stringResource(id = R.string.second), 
-                    image = painterResource(id = R.drawable.lemon_squeeze)
-                ) {
-                    squeezeCount--
-                    if (squeezeCount == 0) {
-                        counter = 3
-                    }
-                }
-            }
-
-            3 -> {
-                LemonTextWithImage(
-                    label = stringResource(id = R.string.third),
-                    image = painterResource(id = R.drawable.lemon_drink)
-                ) {
-                    counter = 4
-                }
-            }
-
-            4 -> {
-                LemonTextWithImage(
-                    label = stringResource(id = R.string.third),
-                    image = painterResource(id = R.drawable.lemon_restart)
-                ) {
-                    counter = 1
-                }
-            }
-        }
-
-    }
-}
-
-@Composable
-fun LemonTextWithImage(label: String, image: Painter, onClickStarted: () -> Unit) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(text = label,
-            fontSize = 18.sp
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Image(
-            painter = image,
-            contentDescription = "imageOne",
-            modifier = Modifier
-                .wrapContentSize()
-                .border(
-                    width = 2.dp,
-                    color = Color(red = 105, green = 205, blue = 216),
-                    shape = RoundedCornerShape(4.dp)
-                )
-                .clickable {
-                    onClickStarted()
-                }
-        )
-    }
+private fun calculateTip(
+    amount: Double,
+    tipPercent: Double = 15.0
+): String {
+    val tip = tipPercent/100 * amount
+    return NumberFormat.getCurrencyInstance().format(tip)
+//    return NumberFormat.getCurrencyInstance().format(tip)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ArticleTheme {
-        LemonadeApp()
+        TipTimeScreen()
     }
 }
